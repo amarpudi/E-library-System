ebbb80163cdb80f93e5ba03453c0e6f97bf3b1d9
amarpudi <amarpudi080@gmail.com>
2026-04-26 18:02:12 +0530

    restructured project

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,6 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const SECRET_KEY = 'super_secret_library_key';
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 const path = require("path");
 const dbPath = path.join(__dirname, "library.sqlite");
 // Middleware
@@ -17,12 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 // Database Setup
-// const sequelize = new Sequelize({
-//   dialect: 'sqlite',
-//   storage: './library.sqlite',
-//   logging: false,
-// });
-
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: dbPath,
@@ -313,3 +304,7 @@ app.get('/api/users/dashboard', authenticateToken, async (req, res) => {
   res.json(records);
 });
 
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
